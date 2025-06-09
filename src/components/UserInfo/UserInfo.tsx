eafc87b583b5acd6aa1246fef4039088f0328d8f
santipop1 <a01663888@tec.mx>
2025-06-09 04:25:09 -0600

    Update UserInfo.tsx

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -9,7 +9,6 @@ const UserInfo = ({ user }: UserInfoProps) => {
     <div className="user-info-admin" style={{ marginBottom: 8, fontSize: 14 }}>
       <div><b>Usuario:</b> {user.firstName} {user.middleName || ''} {user.firstLastName} {user.secondLastName || ''}</div>
       <div><b>Tel:</b> {user.phoneNumber || 'Sin teléfono'}</div>
-      <div><b>Correo Firebase:</b> {user.firebaseId}</div>
       {user.birthday && (
         <div><b>Nacimiento:</b> {new Date(user.birthday).toLocaleDateString()}</div>
       )}
