ec49ad619dab2e1d8be6199550c3bfe3373a25fd
Davidps11 <a01663806@tec.mx>
2025-06-08 18:26:06 -0600

    funcionalidad en formulario

--- src/components/RecuadroFranquicias/RecuadroFranquicias.tsx
+++ src/components/RecuadroFranquicias/RecuadroFranquicias.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { Pencil } from "lucide-react";
 import Image from "next/image";
-import Link from "next/link";
 import "./RecuadroFranquicias.css";
 
 export interface RecuadroFranquiciasProps {
@@ -21,14 +19,6 @@ const RecuadroFranquicias = ({ nombre, logoSrc, onClick }: RecuadroFranquiciasPr
         className="recuadro-logo"
       />
       <span className="recuadro-nombre">{nombre}</span>
-      <Link
-        href="/editar-franquicia"
-        className="recuadro-boton"
-        onClick={(e) => e.stopPropagation()}
-        aria-label="Editar franquicia"
-      >
-        <Pencil size={18} />
-      </Link>
     </div>
   );
 };
